17af05fe50b17005bec0ad2500cf40af54d6965b
Akshay Raj <144332179+AkshayRaj22062002@users.noreply.github.com>
2023-12-21 12:21:08 +0530

    fist

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bytetalk/view/bottomnavigationbar.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Myapp());
+}
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: bottomnavigationbar(),
+    );
+  }
+}
---
+++ lib/view/bottomnavigationbar.dart
@@ -0,0 +1,113 @@
+import 'package:bytetalk/view/Contact.dart';
+import 'package:bytetalk/view/call.dart';
+import 'package:bytetalk/view/message.dart';
+import 'package:bytetalk/view/settings.dart';
+import 'package:flutter/material.dart';
+import 'package:motion_tab_bar_v2/motion-badge.widget.dart';
+import 'package:motion_tab_bar_v2/motion-tab-bar.dart';
+import 'package:motion_tab_bar_v2/motion-tab-controller.dart';
+import 'package:motion_tab_bar_v2/motion-tab-item.dart';
+
+class bottomnavigationbar extends StatefulWidget {
+  const bottomnavigationbar({super.key});
+
+  @override
+  State<bottomnavigationbar> createState() => _bottomnavigationbarState();
+}
+
+class _bottomnavigationbarState extends State<bottomnavigationbar> with TickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: MotionTabBar(
+        
+     // ADD THIS if you need to change your tab programmatically
+    initialSelectedTab: "Call",
+    labels: const ["Message", "Call", "contact", "Settings"],
+    icons: const [Icons.message, Icons.call, Icons.contact_page, Icons.settings],
+    
+
+    // optional badges, length must be same with labels
+    badges: [
+      // Default Motion Badge Widget
+      const MotionBadgeWidget(
+        text: '99+',
+        textColor: Colors.white, // optional, default to Colors.white
+        color: Colors.red, // optional, default to Colors.red
+        size: 18, // optional, default to 18
+      ),
+
+      // custom badge Widget
+      Container(
+        color: Colors.black,
+        padding: const EdgeInsets.all(2),
+        child: const Text(
+          '48',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.white,
+          ),
+        ),
+      ),
+
+      // allow null
+      null,
+
+      // Default Motion Badge Widget with indicator only
+      const MotionBadgeWidget(
+        isIndicator: true,
+        color: Colors.red, // optional, default to Colors.red
+        size: 5, // optional, default to 5,
+        show: true, // true / false
+      ),
+    ],
+    tabSize: 50,
+    tabBarHeight: 55,
+    textStyle: const TextStyle(
+      fontSize: 12,
+      color: Colors.black,
+      fontWeight: FontWeight.w500,
+    ),
+    tabIconColor: Colors.blue[600],
+    tabIconSize: 28.0,
+    tabIconSelectedSize: 26.0,
+    tabSelectedColor: Colors.blue[900],
+    tabIconSelectedColor: Colors.white,
+    tabBarColor: const Color(0xFFAFAFAF),
+    onTabItemSelected: (int value) {
+      setState(() {
+        _tabController.index = value;
+      });
+
+      
+    },
+    
+  ),
+  body: TabBarView(
+    controller: _tabController,
+    
+    children: [
+    message(),
+    call(),
+    contact(),
+    settings()
+    
+    
+    
+  ]),
+    );
+  }
+}
